bc8bc9d163f4e2133b0b7c90780b9871f660a41d
David Diaz <davidcol123dm@gmail.com>
2023-05-16 10:32:43 -0500

    me falto hacer unos cambios en el slice de movies

--- src/store/slices/movies.slice.js
+++ src/store/slices/movies.slice.js
@@ -30,10 +30,10 @@ export const getMoviesThunk = () => (dispatch) => {
 
 export const addMovieThunk = movie => dispatch => {
     return dispatch(genericRequestThunk(async () => {
-        const { data } = await axios.post('/movies', movie);
-        const { data: genres } = await axios.post(`/movies/${data.id}/genres`, movie.genres);
-        const { data: directors } = await axios.post(`/movies/${data.id}/directors`, movie.directors);
-        const { data: actors } = await axios.post(`/movies/${data.id}/actors`, movie.actors);
+        const { data } = await axios.post(VITE_API_URL+'/movies', movie);
+        const { data: genres } = await axios.post(VITE_API_URL+`/movies/${data.id}/genres`, movie.genres);
+        const { data: directors } = await axios.post(VITE_API_URL+`/movies/${data.id}/directors`, movie.directors);
+        const { data: actors } = await axios.post(VITE_API_URL+`/movies/${data.id}/actors`, movie.actors);
         dispatch(addMovie({...data, genres, directors, actors}));
     }, "Movie added successfully"))
 }
